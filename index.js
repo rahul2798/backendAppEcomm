c334face4ce3378d3ce6f61b1933e70cef2afc5f
RAHUL KUMAR <90867352+rahul2798@users.noreply.github.com>
2024-02-12 18:29:00 +0000

    database connected with env

---
+++ index.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import app from "./src/app.js";
+import config from "./src/confif/index.js";
+
+//first thing after starting a backend is to database connection
+//IIFE (SELF INVOKED FUNCTION)
+
+//create a method
+
+//run a method
+( async () =>{
+    try {
+      await mongoose.connect(config.MONGODB_URL)  //--when we have local database
+      console.log("DB CONNECTED !");
+
+      app.on('error', (err) =>{
+        console.log("ERROR:", err);
+        throw err
+      })
+//before listening app.on should be before listening so that error should be first called
+      const onListening = () =>{
+        console.log(`Listening on port ${config.PORT}`)
+      }
+//onListening is callback function
+      app.listen(config.PORT, onListening)
+
+    } catch (err) {
+        console.log("ERROR: ", err)
+        throw err
+    }
+})()
